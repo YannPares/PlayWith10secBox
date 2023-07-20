b90bf1b16b11ae39bf43b34489dfc463cbb2d7a6
Ian Pares <yannpares@gmail.com>
2023-07-20 15:23:13 +0200

    (add): sections in html to changes game windows

---
+++ script.js
@@ -0,0 +1,11 @@
+const frameA = document.getElementById('frame1A');
+const frameB = document.getElementById('frame1B');
+const frameC = document.getElementById('frame1C');
+const frameD = document.getElementById('frame1D');
+frameA.style.display = 'none';
+frameB.style.display = 'none';
+frameC.style.display = 'none';
+// frameD.style.display = 'none';
+
+
+
